df271f943e382d34c7019e293d80298f53fb204c
Ozy-dev <zulfadlifauzi10@gmail.com>
2024-11-28 14:06:21 +0800

    Update proximity_client.py

--- proximity_client.py
+++ proximity_client.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+from beginner_tutorials.srv import ProxBoolean
+
+def proximity_client():
+    rospy.wait_for_service('proximity_sensor')  # Menunggu hingga service tersedia
+    try:
+        # Membuat proxy untuk service
+        get_distance = rospy.ServiceProxy('proximity_sensor', ProxBoolean)
+
+        # Panggil service dan dapatkan jarak
+        response = get_distance()
+        if response.distance == -1.0:
+            rospy.loginfo("Failed to get distance.")  # Jika ada error
+        else:
+            rospy.loginfo(f"Measured Distance: {response.distance} cm")  # Menampilkan jarak
+
+    except rospy.ServiceException as e:
+        rospy.logerr(f"Service call failed: {e}")  # Menangani kesalahan
+
+if __name__ == "__main__":
+    rospy.init_node('proximity_client')  # Inisialisasi node client
+    proximity_client()
 
